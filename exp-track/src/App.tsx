9f79deb015e5e9234ff2042ec7276336e9c8cccf
Carlos Gámez <hardpx@gmail.com>
2024-01-30 19:45:56 -0600

    added delete ist functionality

--- exp-track/src/App.tsx
+++ exp-track/src/App.tsx
@@ -1,19 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 import ExpenseList from './expense-tracker/components/ExpenseList';
 
 function App() {
-  const expenses = [
+  // const handleDelete = (e) => {
+  //   const { target } = e
+  //   setExpenses( prevExpenses => prevExpenses.filter(e => e.id !== id) )
+  // }
+  const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaaaa', amount: 10, category: 'test' },
     { id: 2, description: 'bbbbb', amount: 10, category: 'test' },
     { id: 3, description: 'ccccc', amount: 10, category: 'test' },
     { id: 4, description: 'ddddd', amount: 10, category: 'test' },
     { id: 5, description: 'eeeee', amount: 10, category: 'test' },
-  ];
+  ]);
+
   return (
     <>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log('Deleted', id)}
+        onDelete={(id) =>
+          setExpenses((expenses) => expenses.filter((e) => e.id !== id))
+        }
       />
     </>
   );
