2413656f4fd64c54adbd3acb5dd59215cf3c2ba4
Carlos Gámez <hardpx@gmail.com>
2024-04-01 17:57:50 -0600

    “setup”

---
+++ modern-react-redux/r-comps/src/App.jsx
@@ -0,0 +1,14 @@
+import ProfileCard from './components/ProfileCard';
+
+function App() {
+  return (
+    <>
+      <h1>Peronsal Digital Assitants</h1>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </>
+  );
+}
+
+export default App;
