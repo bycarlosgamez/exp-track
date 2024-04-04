0b17730bfedf9e2bdc0efaa6daf9aba7cd6603f3
Carlos Gámez <hardpx@gmail.com>
2024-04-03 20:37:45 -0600

    added bulma css

--- modern-react-redux/r-comps/src/App.jsx
+++ modern-react-redux/r-comps/src/App.jsx
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './components/ProfileCard';
 import AlexaImg from './assets/images/alexa.png';
 import CortanaImg from './assets/images/cortana.png';
@@ -7,9 +8,23 @@ function App() {
   return (
     <>
       <h1>Peronsal Digital Assitants</h1>
-      <ProfileCard title='Alexa' handle='@hanlde' img={AlexaImg} />
-      <ProfileCard title='Cortana' handle='@hanlde' img={CortanaImg} />
-      <ProfileCard title='Siri' handle='@hanlde' img={SiriImg} />
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-4'>
+              {' '}
+              <ProfileCard title='Alexa' handle='@hanlde' img={AlexaImg} />
+            </div>
+            <div className='column is-4'>
+              <ProfileCard title='Cortana' handle='@hanlde' img={CortanaImg} />
+            </div>
+            <div className='column is-4'>
+              {' '}
+              <ProfileCard title='Siri' handle='@hanlde' img={SiriImg} />
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
--- modern-react-redux/r-comps/src/components/ProfileCard.jsx
+++ modern-react-redux/r-comps/src/components/ProfileCard.jsx
@@ -1,9 +1,29 @@
 function ProfileCard({ title, handle, img }) {
   return (
     <>
-      <h2>{title}</h2>
-      <p>{handle}</p>
-      <img src={img} />
+      <div className='card'>
+        <div className='card-image'>
+          <figure className='image is-1by1'>
+            <img src={img} alt='Placeholder image' />
+          </figure>
+        </div>
+        <div className='card-content'>
+          <div className='media'>
+            <div className='media-content'>
+              <p className='title is-4'>{title}</p>
+              <p className='subtitle is-6'>{handle}</p>
+            </div>
+          </div>
+          {/* 
+          <div className='content'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            nec iaculis mauris. <a>@bulmaio</a>. <a href='#'>#css</a>
+            <a href='#'>#responsive</a>
+            <br />
+            <time datetime='2016-1-1'>11:09 PM - 1 Jan 2016</time>
+          </div> */}
+        </div>
+      </div>
     </>
   );
 }
